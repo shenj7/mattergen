shenj7/mattergen

commit a4935de1072d53866999031df85cb3749354ee4f

batch unconditional and conditional scores (#203)
Authored-by: Daniel Zuegner <dzuegner@microsoft.com>

mattergen/diffusion/sampling/classifier_free_guidance.py
@@ -6,6 +6,7 @@ from typing import Callable
 import torch
 
 from mattergen.diffusion.sampling.pc_sampler import Diffusable, PredictorCorrector
+from mattergen.common.data.collate import collate
 
 BatchTransform = Callable[[Diffusable], Diffusable]
 
@@ -71,9 +72,16 @@ class GuidedPredictorCorrector(PredictorCorrector):
             return get_unconditional_score()
         else:
             # guided_score = guidance_factor * conditional_score + (1-guidance_factor) * unconditional_score
+            batch_no_condition = self._remove_conditioning_fn(x)
+            batch_with_condition = self._keep_conditioning_fn(x)
+            joint_batch = collate([batch_no_condition, batch_with_condition])
+            combined_score = super(GuidedPredictorCorrector, self)._score_fn(
+                x=joint_batch, t=torch.cat([t, t], dim=0),
+            )
+            # Split the combined score back into unconditional and conditional parts.
+            unconditional_score = combined_score[0]
+            conditional_score = combined_score[1]
 
-            conditional_score = get_conditional_score()
-            unconditional_score = get_unconditional_score()
             return unconditional_score.replace(
                 **{
                     k: torch.lerp(

commit fcc7aee7fbf9c8d9932edc36f5e63a9a28314783

fix GuidedPredictorCorrector to handle properties: list when concatenating (un)conditional scores in a batch
This PR fixes a bug introduced in [#203](https://github.com/microsoft/mattergen/pull/203), which effects conditional generation on properties that are of type `list`. This includes the pretrained chemical system models. This PR also extends `TargetProperty` to include `str` as a valid value type, since the chemical system embedding class accepts either `str` or `list[str]` values.

mattergen/common/data/collate.py
@@ -4,6 +4,7 @@
 import warnings
 from typing import Any, Callable, Iterable, Iterator, Sequence, TypeVar, overload
 
+
 from torch import Tensor
 from torch_geometric.data import Batch, Data
 from typing_extensions import TypeGuard

mattergen/common/data/types.py
@@ -11,7 +11,7 @@ from emmet.core.material import PropertyOrigin
 from mattergen.common.utils.globals import PROPERTY_SOURCE_IDS
 
 PropertySourceId = str
-TargetProperty = dict[PropertySourceId, int | float | Sequence[str]]
+TargetProperty = dict[PropertySourceId, int | float | str | Sequence[str]]
 
 
 @dataclass(frozen=True)

mattergen/diffusion/sampling/classifier_free_guidance.py
@@ -75,10 +75,18 @@ class GuidedPredictorCorrector(PredictorCorrector):
             batch_no_condition = self._remove_conditioning_fn(x)
             batch_with_condition = self._keep_conditioning_fn(x)
             joint_batch = collate([batch_no_condition, batch_with_condition])
+
+            for attr,value in batch_no_condition.items():
+                if isinstance(value, list):
+                    joint_batch[attr] = batch_no_condition[attr]+batch_with_condition[attr]
+
+
             combined_score = super(GuidedPredictorCorrector, self)._score_fn(
                 x=joint_batch, t=torch.cat([t, t], dim=0),
             )
             # Split the combined score back into unconditional and conditional parts.
+            # Any batch.attr: list fields will be wrong here because of the manual concatenation above
+            # this should be ok as self._multi_corruption.corrupted_fields are always torch.Tensor
             unconditional_score = combined_score[0]
             conditional_score = combined_score[1]
 