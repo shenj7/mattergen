667556f3649ba9270350d138479680a3b7aa5e54
Yexiang Xue <yexiang@purdue.edu>
2025-10-25 20:09:28 +0000

    Trying to run mattersim_pred.bash that Jackson wrote. Run into some problems.
     On branch experimental-info
     Your branch is up to date with 'origin/experimental-info'.
    
     Changes to be committed:
    	new file:   xyx/results/ml_bulk_modulus400/mattersim_pred_results.txt
    	new file:   xyx/results/ml_bulk_modulus400/metrics.json
    	new file:   xyx/scripts/mattersim_pred.bash
    	new file:   xyx/scripts/pull_models.bash

---
+++ xyx/scripts/mattersim_pred.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../..
+
+export RESULTS_PATH=xyx/results/ml_bulk_modulus400
+
+python mattersim_pred_by_zip.py "$RESULTS_PATH/generated_crystals_cif.zip" "$RESULTS_PATH/qha_outputs" --t-report 300 > "$RESULTS_PATH/mattersim_pred_results.txt"
+
+# for i in $(seq 40 40 400);
+# do
+# 	export RESULTS_PATH="results/$MODEL_NAME/$i"  # Samples will be written to this directory, e.g., `results/dft_mag_density`
+# 	python mattersim_pred_by_zip.py "$RESULTS_PATH/generated_crystals_cif.zip" "$RESULTS_PATH/qha_outputs" --t-report 300 > "$RESULTS_PATH/mattersim_pred_results.txt"
+# done
---
+++ xyx/scripts/pull_models.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#git lfs pull -I checkpoints/ml_bulk_modulus --exclude=""
+git lfs pull -I checkpoints/mattergen_base --exclude=""
