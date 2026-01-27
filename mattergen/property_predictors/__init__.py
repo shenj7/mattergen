81f5a8d0f98c7cef2ad89493ad5bac991cde72a6
Jackson Shen <shenj7@rose-hulman.edu>
2026-01-27 00:17:56 -0500

    export fix

--- mattergen/property_predictors/__init__.py
+++ mattergen/property_predictors/__init__.py
@@ -8,8 +8,15 @@ predict target material properties for guidance or analysis.
 from mattergen.property_predictors.bulk_modulus_time_classifier import (
     BulkModulusTimeClassifier,
 )
+from mattergen.property_predictors.bulk_modulus_time_lora_mlp_predictor import (
+    BulkModulusLoRAMLPTimePredictor,
+)
 from mattergen.property_predictors.bulk_modulus_time_lora_predictor import (
     BulkModulusLoRATimePredictor,
 )
 
-__all__ = ["BulkModulusTimeClassifier", "BulkModulusLoRATimePredictor"]
+__all__ = [
+    "BulkModulusTimeClassifier",
+    "BulkModulusLoRATimePredictor",
+    "BulkModulusLoRAMLPTimePredictor",
+]
